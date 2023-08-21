e64974f698ffcfd25bae56642d14fc5be3800113
Muhammad Ramadhan <muhrama082@gmail.com>
2023-08-21 16:04:12 +0700

    Sedang mengerjakan fitur menampilkan daftar tugas di pengguna

--- app/Http/Controllers/User/TaskController.php
+++ app/Http/Controllers/User/TaskController.php
@@ -15,13 +15,121 @@ class TaskController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         $data['application'] = Cache::rememberForever('application', function () {
             return DB::table('applications')->first();
         });
 
         $data['navbarActive'] = 'tasks';
+
+        $data['input'] = $request->all();
+
+        if (empty($data['input'])) {
+            $data['total'] = DB::table('tasks')->where('user_id', '=', $request->get('userAuth')->id)
+                                                ->count();
+
+            $data['input'] = [
+                'page' => 1,
+                'page_total' => ceil($data['total'] / 12),
+                'year' => null,
+                'month' => null,
+                'day' => null
+            ];
+
+            $data['items'] = DB::table('tasks')->where('user_id', '=', $request->get('userAuth')->id)
+                                                ->orderBy('created_at', 'desc')
+                                                ->offset(0)
+                                                ->limit(12)
+                                                ->get();
+        } else {
+            $data['input']['page'] = $request->page;
+
+            if (empty($data['input']['page'])) {
+                $data['input']['page'] = 1;
+            } else {
+                $data['input']['page'] = intval($data['input']['page']);
+            }
+
+            $data['input']['offset'] = $data['input']['page'] > 1 ? ($data['input']['page'] * 12) - 12 : 0;
+            
+            if (isset($request->month) || isset($request->day)) {
+                if (empty($request->year)) {
+                    $lastData = DB::table('tasks')->select('created_at')
+                                                    ->where('user_id', '=', $request->get('userAuth')->id)
+                                                    ->orderByDesc('created_at')
+                                                    ->first();
+    
+                    $data['input']['year'] = intval(date('Y', $lastData->created_at));
+                } else {
+                    $data['input']['year'] = intval($data['input']['year']);
+                }
+            } else {
+                if (strtotime($request->year)) {
+                    $data['input']['year'] = intval($request->year);
+                } else {
+                    $lastData = DB::table('tasks')->select('created_at')
+                                                    ->where('user_id', '=', $request->get('userAuth')->id)
+                                                    ->orderByDesc('created_at')
+                                                    ->first();
+    
+                    $data['input']['year'] = intval(date('Y', $lastData->created_at));
+                }
+            }
+
+            if (isset($request->day)) {
+                if (empty($request->month)) {
+                    if (empty($lastData)) {
+                        $lastData = DB::table('tasks')->select('created_at')
+                                                        ->where('user_id', '=', $request->get('userAuth')->id)
+                                                        ->orderByDesc('created_at')
+                                                        ->first();
+                    }
+    
+                    $data['input']['month'] = intval(date('m', $lastData->created_at));
+                } else {
+                    $data['input']['month'] = intval($data['input']['month']);
+                }
+            } else {
+                if (strtotime($data['input']['year'] . '-' . $request->month)) {
+                    $data['input']['month'] = intval($request->month);
+                } else {
+                    $data['input']['month'] = null;
+                }
+            }
+
+            if (empty($request->day)) {    
+                $data['input']['day'] = null;
+            } else {
+                $data['input']['day'] = intval($request->day);
+
+                if (!$data['input']['day']) {
+                    $data['input']['day'] = null;
+                }
+            }
+
+            if (!empty($data['input']['year']) && empty($data['input']['month']) && empty($data['input']['day'])) {
+                $stringYear = strval($data['input']['year']);
+
+                $yearMonthTime = strtotime($stringYear . '-12');
+
+                $lastDay = date('t', $yearMonthTime);
+
+                $lastDateTime = strtotime($stringYear . '-12-' . $lastDay . ' 23:59:59');
+
+                $startDateTime = strtotime($stringYear . '-01-01' . ' 00:00:00');
+
+                $data['items'] = DB::table('tasks')->select('id', 'title', 'description')
+                                                    ->where('user_id', '=', $request->get('userAuth')->id)
+                                                    ->whereBetween('created_at', [$startDateTime - 1, $lastDateTime + 1])
+                                                    ->orderByDesc('created_at')
+                                                    ->offset($data['input']['offset'])
+                                                    ->limit(12)
+                                                    ->get();
+            }
+        }
+
+        dd($data['items']);
 
         return view('pages.user.task.index', $data);
     }
